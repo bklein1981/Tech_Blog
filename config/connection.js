7011cab458a060dea1cb1c97ca74738fec6b3565
Brian Klein <bklein1981@gmail.com>
2023-09-06 16:34:32 -0500

    added server code and configuration code

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//Modules
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+//set sequelize as variable
+let sequelize;
+
+//Setup database connection using sequelize
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306,
+    }
+  );
+}
+
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,53 @@
+//import required modules/routes/middleware
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+//importing sequelize  & sequelizeStore
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+//setting var app to use express and port
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+//creating instance of handlebar
+const hbs = exphbs.create();
+
+//setting up session
+const sess = {
+    secret: 'Secret Key for the tech_blog session',
+    cookie: {
+        maxAge: 300000,
+        httpOnly: true,
+        secure: false,
+        sameSite: 'strict',
+    },
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+};
+
+//setting the app to use session of sess
+app.use(session(sess));
+
+//calling template engine
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+//routes
+app.use(routes);
+
+//syncing sequelize and starting the express server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log('Now listening'));
+});
